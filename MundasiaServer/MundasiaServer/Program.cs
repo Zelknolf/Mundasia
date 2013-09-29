57dc058d8fc5f43ee1d720486052d5c8163217e7
Zelknolf <zelknolf@yahoo.com>
2013-09-29 18:30:01 -0500

    Mundasia server executable. Currently only hosts the connector

---
+++ MundasiaServer/MundasiaServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+using Mundasia.Server.Communication;
+
+namespace Mundasia.Server
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Service.Open();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Status());
+            Service.Close();
+        }
+    }
+}
