926df0e644245f1c5ee72158adae68366fffa884
Zelknolf <zelknolf@yahoo.com>
2013-09-29 18:29:28 -0500

    Server connector class. Contract only includes a ping, which returns the server time as utc.

---
+++ ServerConnector/ServerConnector/IServerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Mundasia.Server.Communication
+{
+    [ServiceContract]
+    public interface IServerService
+    {
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml)]
+        string Ping();
+    }
+}
---
+++ ServerConnector/ServerConnector/ServerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Mundasia.Server.Communication
+{
+    class ServerService : IServerService
+    {
+        public string Ping()
+        {
+            return DateTime.UtcNow.ToShortTimeString();
+        }
+    }
+}
