cebd1b9b8d406304dcd81b08dd634ad0eac22d27
Zelknolf <zelknolf@yahoo.com>
2013-10-03 23:50:35 -0500

    Provide a client connector object, with the basics necessary to connect to a server.

---
+++ ClientConnector/ClientConnector/ServiceConsumer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Mundasia.Communication
+{
+    public class ServiceConsumer
+    {
+        // TODO: Move this to app.config
+        public static string baseServerTarget = "";
+
+        public static string Ping()
+        {
+            try
+            {
+                string wrURI = baseServerTarget + "ping";
+                WebRequest wreq = WebRequest.Create(wrURI);
+                WebResponse wresp = wreq.GetResponse();
+                using (StreamReader sr = new StreamReader(wresp.GetResponseStream()))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch
+            {
+                return String.Empty;
+            }
+        }
+    }
+}
