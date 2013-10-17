0609698421dc4eef88ece7fe30ed39cacd310dce
Zelknolf <zelknolf@yahoo.com>
2013-10-16 22:21:44 -0500

    Server half of account creation

--- ServerConnector/ServerConnector/IServerService.cs
+++ ServerConnector/ServerConnector/IServerService.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using System.ServiceModel;
 using System.ServiceModel.Web;
+using System.Security.Cryptography;
 
 namespace Mundasia.Server.Communication
 {
@@ -12,7 +13,15 @@ namespace Mundasia.Server.Communication
     public interface IServerService
     {
         [OperationContract]
-        [WebGet(BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml)]
+        [WebGet(BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml)]
         string Ping();
+
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml)]
+        RSAParameters GetPublicKey();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate="CreateAccount?message={message}", ResponseFormat = WebMessageFormat.Xml)]
+        bool CreateAccount(string message);
     }
 }
--- ServerConnector/ServerConnector/ServerService.cs
+++ ServerConnector/ServerConnector/ServerService.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Mundasia.Communication;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
 
 namespace Mundasia.Server.Communication
 {
-    class ServerService : IServerService
+    public class ServerService : IServerService
     {
         public string Ping()
         {
             return DateTime.UtcNow.ToShortTimeString();
+        }
+
+        public RSAParameters GetPublicKey()
+        {
+            return Encryption.GetPubKey();
+        }
+
+        public bool CreateAccount(string message)
+        {
+            int startTrim = message.IndexOf("xmlns");
+            int endTrim = message.IndexOf(">");
+            message = message.Remove(startTrim, endTrim - startTrim);
+            MemoryStream stream = new MemoryStream();
+            StreamWriter write = new StreamWriter(stream);
+            write.Write(message);
+            write.Flush();
+            stream.Position = 0;
+            XmlSerializer xml = new XmlSerializer(typeof(AccountCreation));
+            AccountCreation deSerMessage = (AccountCreation)xml.Deserialize(stream);
+            try
+            {
+                string decMessage = Encryption.Decrypt(deSerMessage.message, deSerMessage.pubKey);
+                decMessage = decMessage.Replace("\0", "");
+                char[] split = new char[] { '\n' };
+                string[] credentials = decMessage.Split(split, 2);
+                if (new Account(credentials[0], credentials[1]) != null)
+                {
+                    return true;
+                }
+            }
+            catch {}
+            return false;
         }
     }
 }
