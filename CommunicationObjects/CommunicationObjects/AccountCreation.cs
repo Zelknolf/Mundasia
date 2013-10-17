382f804a0751f1e7f080cd589679f3987d4d9a52
Zelknolf <zelknolf@yahoo.com>
2013-10-16 22:16:22 -0500

    CommunicationObjects to define objects meant for passing between client and server, to help enforce what information is passed and simplify (de)serialization

---
+++ CommunicationObjects/CommunicationObjects/AccountCreation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Mundasia.Communication
+{
+    public class AccountCreation
+    {
+        public byte[] message;
+        public RSAParameters pubKey;
+    }
+}
