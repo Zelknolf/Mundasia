2e15cd3034424c288c139ef0105b71aa9e3b8bc2
Zelknolf <Zelknolf@yahoo.com>
2013-10-30 19:42:37 -0500

    ServiceConsumer also uses AccountCreation's ToString / Constructor. New accounts now reliable.

--- ClientConnector/ClientConnector/ServiceConsumer.cs
+++ ClientConnector/ClientConnector/ServiceConsumer.cs
@@ -12,7 +12,7 @@ namespace Mundasia.Communication
 {
     public class ServiceConsumer
     {
-        public const string RSAParametersNamespace = "http://schemas.datacontract.org/2004/07/System.Security.Cryptography";
+        public const string StringNamespace = "http://schemas.microsoft.com/2003/10/Serialization/";
         
         // TODO: Move this to app.config
         public static string baseServerTarget = "http://localhost:6200/MundasiaServerService/";
@@ -37,7 +37,7 @@ namespace Mundasia.Communication
 
         public static string CreateAccount(string userName, string password)
         {
-            RSAParameters key;
+            AccountCreation ac;
             try
             {
                 string wrURI = baseServerTarget + "getpublickey";
@@ -45,31 +45,30 @@ namespace Mundasia.Communication
                 WebResponse wresp = wreq.GetResponse();
                 using (StreamReader sr = new StreamReader(wresp.GetResponseStream()))
                 {
-                    XmlSerializer rsa = new XmlSerializer(typeof(RSAParameters), RSAParametersNamespace);
-                    key = (RSAParameters)rsa.Deserialize(sr);
+                    XmlSerializer rsa = new XmlSerializer(typeof(string), StringNamespace);
+                    string resp = (string)rsa.Deserialize(sr);
+                    ac = new AccountCreation(resp);
                 }
             }
             catch
             {
                 return "Error.";
             }
-            byte[] message = Encryption.Encrypt(String.Format("{0}\n{1}", userName, password), key);
+            ac.message = Encryption.Encrypt(String.Format("{0}\n{1}", userName, password), ac.pubKey);
 
             try
             {
                 string wrURI = baseServerTarget + "createaccount";
-                XmlSerializer xml = new XmlSerializer(typeof(AccountCreation));
-                StringWriter strwrt = new StringWriter();                
-                xml.Serialize(strwrt, new AccountCreation() { message = message, pubKey = key });
-                string msg = strwrt.ToString();
-                msg = msg.Substring(msg.IndexOf('>') + 1);
+                string msg = ac.ToString();
                 WebRequest wreq = WebRequest.Create(wrURI + "?message=" + msg);
                 wreq.Method = "POST";
                 wreq.ContentLength = 0;
                 WebResponse wresp = wreq.GetResponse();
                 using (TextReader sr = new StreamReader(wresp.GetResponseStream()))
                 {
-                    return sr.ReadToEnd();
+                    XmlSerializer xml = new XmlSerializer(typeof(bool), StringNamespace);
+                    bool resp = (bool)xml.Deserialize(sr);
+                    return resp.ToString();
                 }
 
             }
