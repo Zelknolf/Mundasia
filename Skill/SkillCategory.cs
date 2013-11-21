43e11c97ac781d788032c790e328b1ec9cbd14ba
Zelknolf <Zelknolf@yahoo.com>
2013-11-20 20:18:00 -0600

    Project to load and manage skill objects

---
+++ Skill/SkillCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mundasia.Objects
+{
+    public enum SkillCategory
+    {
+        Combat,
+        Artisan,
+        Scholarship,
+        Survival,
+        Social,
+        Supernatural,
+
+        Error
+    }
+}
