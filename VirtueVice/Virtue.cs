cc7ddb19080cbe8527ac18b4a3e20b7b1d36b38b
Zelknolf <Zelknolf@yahoo.com>
2013-11-20 20:15:00 -0600

    Project to load and provide Virtues and Vices

---
+++ VirtueVice/Virtue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mundasia.Objects
+{
+    public class Virtue
+    {
+        public Virtue(string fileLine)
+        {
+            string[] split = fileLine.Split(new char[] { '|' });
+            uint index = 0;
+            if (uint.TryParse(split[0], out index) &&
+                !_library.ContainsKey(index))
+            {
+                Id = index;
+                Name = split[1];
+                if(!uint.TryParse(split[2], out Description))
+                {
+                    Description = uint.MaxValue;
+                }
+            }
+        }
+
+        public uint Id;
+        public uint Description;
+        public string Name;
+
+
+        public static Dictionary<uint, Virtue> _library = new Dictionary<uint, Virtue>();
+
+        public static void Load()
+        {
+            string file = System.IO.Directory.GetCurrentDirectory() + "\\DataArrays\\Virtues.txt";
+            FileStream strLib = File.Open(file, FileMode.Open);
+            using (StreamReader read = new StreamReader(strLib))
+            {
+                while (read.Peek() >= 0)
+                {
+                    Virtue toAdd = new Virtue(read.ReadLine());
+                    _library.Add(toAdd.Id, toAdd);
+                }
+            }
+        }
+
+        public static Virtue GetVirtue(uint index)
+        {
+            if(_library.ContainsKey(index))
+            {
+                return _library[index];
+            }
+            return null;
+        }
+    }
+}
