53c138c09ffc3294f5c9e6d1cebdbc9e4aba0d1b
Zelknolf <Zelknolf@yahoo.com>
2014-02-01 23:24:24 -0600

    Start the concept of a map as a thing that can contain tiles.

---
+++ Tiles/Tiles/Map.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mundasia.Objects
+{
+    public class Map
+    {
+        /// <summary>
+        /// A collection of all of the tiles in the area. Tiles appear as Tiles[X][Y][Z].
+        /// </summary>
+        public Dictionary<int, Dictionary<int, Dictionary<int, Tile>>> Tiles = new Dictionary<int, Dictionary<int, Dictionary<int, Tile>>>();
+
+        /// <summary>
+        /// Storage of whether or not a given X-Y-Z/1000 coordinate has had its stack loaded.
+        /// (all Z coordinates from 0-999 are "0" in this dictionary, per Tile.LoadStack's behavior.)
+        /// </summary>
+        public Dictionary<int, Dictionary<int, Dictionary<int, bool>>> TilesLoaded = new Dictionary<int, Dictionary<int, Dictionary<int, bool>>>();
+
+        /// <summary>
+        /// The name of the map being built.
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Add a tile to this map.
+        /// </summary>
+        /// <param name="ToAdd">The tile to add</param>
+        /// <returns>true if the tile is added; false on error</returns>
+        public bool Add(Tile ToAdd)
+        {
+            if(ToAdd.PosX > 40000000 || // 40,000 km. Roughly the circumference of the Earth.
+               ToAdd.PosX < 0 ||        // -1 = 40000000
+               ToAdd.PosY > 40000000 ||
+               ToAdd.PosY < 0 ||
+               ToAdd.PosZ < -50000 ||   // thickness of the crust
+               ToAdd.PosZ > 8000)       // atmosphere too thin to support life
+            {
+                return false;
+            }
+            int tileHeight = ToAdd.TileHeight;
+            while (tileHeight > 1)
+            {
+                tileHeight--;
+                if (GetTileExact(ToAdd.PosX, ToAdd.PosY, ToAdd.PosZ - tileHeight) != null)
+                {
+                    return false;
+                }
+            }
+            if(GetTileOverlap(ToAdd.PosX, ToAdd.PosY, ToAdd.PosZ) != null)
+            {
+                return false;
+            }
+            if(!Tiles.ContainsKey(ToAdd.PosX))
+            {
+                Tiles.Add(ToAdd.PosX, new Dictionary<int, Dictionary<int, Tile>>());
+            }
+            if(!Tiles[ToAdd.PosX].ContainsKey(ToAdd.PosY))
+            {
+                Tiles[ToAdd.PosX].Add(ToAdd.PosY, new Dictionary<int, Tile>());
+            }
+            if(!Tiles[ToAdd.PosX][ToAdd.PosY].ContainsKey(ToAdd.PosZ))
+            {
+                Tiles[ToAdd.PosX][ToAdd.PosY].Add(ToAdd.PosZ, ToAdd);
+                return true;
+            }
+            else
+            {
+                Tiles[ToAdd.PosX][ToAdd.PosY][ToAdd.PosZ] = null;
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Remove a tile from this map.
+        /// </summary>
+        /// <param name="ToRemove">The tile to remove</param>
+        /// <returns>True on success; false if the tile is not found</returns>
+        public bool Remove(Tile ToRemove)
+        {
+            if(!Tiles.ContainsKey(ToRemove.PosX))
+            {
+                return false;
+            }
+            if(!Tiles[ToRemove.PosX].ContainsKey(ToRemove.PosY))
+            {
+                return false;
+            }
+            if(!Tiles[ToRemove.PosX][ToRemove.PosY].ContainsKey(ToRemove.PosZ))
+            {
+                return false;
+            }
+            Tiles[ToRemove.PosX][ToRemove.PosY].Remove(ToRemove.PosZ);
+            if(Tiles[ToRemove.PosX][ToRemove.PosY].Count == 0)
+            {
+                Tiles[ToRemove.PosX].Remove(ToRemove.PosY);
+            }
+            if(Tiles[ToRemove.PosX].Count == 0)
+            {
+                Tiles.Remove(ToRemove.PosX);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Finds a tile that is placed exactly at the X/Y/Z coordinates provided.
+        /// </summary>
+        /// <param name="X">The X coordinate</param>
+        /// <param name="Y">The Y coordinate</param>
+        /// <param name="Z">The Z coordinate</param>
+        /// <returns>The tile, or null if one isn't found.</returns>
+        public Tile GetTileExact(int X, int Y, int Z)
+        {
+            if (!Tiles.ContainsKey(X))
+            {
+                return null;
+            }
+            if (!Tiles[X].ContainsKey(Y))
+            {
+                return null;
+            }
+            if (Tiles[X][Y].ContainsKey(Z))
+            {
+                return Tiles[X][Y][Z];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Returns any tile that
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <param name="Z"></param>
+        /// <returns></returns>
+        public Tile GetTileOverlap(int X, int Y, int Z)
+        {
+            Tile exact = GetTileExact(X, Y, Z);
+            if (exact != null) return exact;
+            if(Tiles[X][Y].ContainsKey(Z+1))
+            {
+                Tile toCheck = Tiles[X][Y][Z + 1];
+                if(toCheck.TileHeight > 1)
+                {
+                    return toCheck;
+                }
+            }
+            if (Tiles[X][Y].ContainsKey(Z + 2))
+            {
+                Tile toCheck = Tiles[X][Y][Z + 2];
+                if (toCheck.TileHeight > 2)
+                {
+                    return toCheck;
+                }
+            }
+            if (Tiles[X][Y].ContainsKey(Z + 3))
+            {
+                Tile toCheck = Tiles[X][Y][Z + 3];
+                if (toCheck.TileHeight > 3)
+                {
+                    return toCheck;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// This method will load all of the tiles within 40 horizontal tiles of the center point, and within the 1000-tall block of tiles.
+        /// </summary>
+        /// <param name="X">The X coordinate at the center of the loading.</param>
+        /// <param name="Y">The Y coordinate at the center of the loading.</param>
+        /// <param name="Z">The Z coordinate at the center of the loading.</param>
+        /// <returns></returns>
+        public void LoadNearby(int X, int Y, int Z)
+        {
+            int ZforPath = Z;
+            Z = Z / 1000;
+            for (int c = X - 40; c < X + 40; c++)
+            {
+                for(int cc = Y - 40; cc < Y + 40; cc++)
+                {
+                    if(!TilesLoaded.ContainsKey(X) ||
+                       !TilesLoaded[X].ContainsKey(Y) ||
+                       !TilesLoaded[X][Y].ContainsKey(Z) ||
+                       !TilesLoaded[X][Y][Z])
+                    {
+                        List<Tile> nearby = Tile.LoadStack(X, Y, ZforPath);
+                        foreach(Tile tile in nearby)
+                        {
+                            if(Tiles.ContainsKey(X) &&
+                               Tiles[X].ContainsKey(Y) &&
+                               Tiles[X][Y].ContainsKey(tile.PosZ))
+                            {
+                                continue;
+                            }
+                            if(!Tiles.ContainsKey(X))
+                            {
+                                Tiles.Add(X, new Dictionary<int, Dictionary<int, Tile>>());
+                            }
+                            if(!Tiles[X].ContainsKey(Y))
+                            {
+                                Tiles[X].Add(Y, new Dictionary<int, Tile>());
+                            }
+                            Tiles[X][Y].Add(tile.PosZ, tile);
+                        }
+                        if(!TilesLoaded.ContainsKey(X))
+                        {
+                            TilesLoaded.Add(X, new Dictionary<int, Dictionary<int, bool>>());
+                        }
+                        if(!TilesLoaded[X].ContainsKey(Y))
+                        {
+                            TilesLoaded[X].Add(Y, new Dictionary<int, bool>());
+                        }
+                        if(!TilesLoaded[X][Y].ContainsKey(Z))
+                        {
+                            TilesLoaded[X][Y].Add(Z, true);
+                        }
+                        else
+                        {
+                            TilesLoaded[X][Y][Z] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        public List<Tile> GetNearby(int X, int Y, int Z)
+        {
+            LoadNearby(X, Y, Z);
+            List<Tile> ret = new List<Tile>();
+            for (int c = X - 40; c < X + 40; c++)
+            {
+                for (int cc = Y - 40; cc < Y + 40; cc++)
+                {
+                    // Because Tile.LoadStack will provide an empty collection if there's nothing to load, we can assume
+                    // that LoadNearby will have put tiles into all of these places.
+                    foreach(Tile tile in Tiles[X][Y].Values)
+                    {
+                        if(Math.Abs(tile.PosZ - Z) < 1000)
+                        {
+                            ret.Add(tile);
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
